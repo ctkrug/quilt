2d9d4fefa31fe5bf6aefb99b8724216d54035d7c
ctkrug <ctkrug4501@gmail.com>
2026-07-02 01:35:46 +0000

    feat(pkg): export load_events_from_rows from the top-level package

--- src/habit_heatmap/__init__.py
+++ src/habit_heatmap/__init__.py
@@ -1,7 +1,7 @@
 """habit-heatmap: render GitHub-style contribution heatmaps from any CSV of dated events."""
 
-from .parser import load_events
+from .parser import load_events, load_events_from_rows
 from .render_svg import render_svg
 from .version import __version__
 
-__all__ = ["load_events", "render_svg", "__version__"]
+__all__ = ["load_events", "load_events_from_rows", "render_svg", "__version__"]
